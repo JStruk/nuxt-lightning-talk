38b342db074b3f44e7334730f9bd8be8d6b02be0
JStruk <strukjustin@gmail.com>
2022-07-14 13:49:21 -0400

    implement data-fetching page

---
+++ components/UsersTable.vue
@@ -0,0 +1,25 @@
+<template>
+    <table>
+        <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+        </tr>
+
+        <tr v-for="user in props.users" v-bind:key="user.index">
+            <td>{{ user.index }}</td>
+            <td>{{ user.name }}</td>
+            <td>{{ user.email }}</td>
+            <td>{{ user.phone }}</td>
+        </tr>
+    </table>
+</template>
+
+<script setup>
+const props = defineProps({
+    users: { type: Array, required: true }
+})
+
+console.log(props.users)
+</script>
---
+++ pages/data-fetching.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1> Data Fetching yaaay</h1>
+        <p> {{ data }}</p>
+
+        <UsersTable :users='users' />
+
+        <hr />
+        <div>
+            <span> Counter value is: {{ counter }}</span>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+const { pending, data: users, refresh, errors } = useLazyFetch('/api/users')
+
+const counter = useState('counter');
+
+</script>
