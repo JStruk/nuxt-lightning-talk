2610c73e4077e1a3b4c476c3e0df816158f2d76c
JStruk <strukjustin@gmail.com>
2022-07-14 13:47:53 -0400

    use vue-router

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    <h1>NuxtJS 3 Lightning Talk</h1>
+
+    <NuxtLink to="/data-fetching">Data Fetching</NuxtLink>
+    <!-- <button @click="dataFetchingButtonClicked"> Data Fetching </button> -->
+    <NuxtLink to="/state-management">State Management</NuxtLink>
+    <NuxtLink to="/head-management"> Head management </NuxtLink>
+
+  </div>
+</template>
+
+<script setup>
+const router = useRouter();
+
+</script>
